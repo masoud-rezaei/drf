9912e2f6455b749829fd8d5b7df83fe5ba8ff3cf
Your Namecls <you@example.com>
2022-04-14 02:46:11 +0430

    'Authentication'

--- config/settings.py
+++ config/settings.py
@@ -37,8 +37,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
     #3d party apps
     'rest_framework',
+    'rest_framework.authtoken',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'dj_rest_auth',
+    'dj_rest_auth.registration',
+
+
     #local
     'posts',
 ]
@@ -130,5 +139,16 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK={
     'DEFAULT_PERMISSION_CLASSES':[
         'rest_framework.permissions.IsAuthenticated'
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES':[
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+
     ]
 }
+
+##Email
+
+Email_BACKEND="django.core.mail.backends.console.EmailBackend"
+
+SITE_ID=1
