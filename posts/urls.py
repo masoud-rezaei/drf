32391aa1c4bfb2e4cd278476b2cd1b861f56d7e7
Your Namecls <you@example.com>
2022-04-15 22:23:29 +0430

    'schema

--- posts/urls.py
+++ posts/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 from rest_framework.routers import SimpleRouter
-from .views import HomePageView  ,UserViewSet,PostViewSet
+from .views import UserViewSet,PostViewSet
 
 router=SimpleRouter()
 router.register('users',UserViewSet,basename="users")
 router.register('',PostViewSet,basename="posts")
-#router.register('',HomePageView.as_view(),basename="home")
 
 urlpatterns=router.urls
--- posts/views.py
+++ posts/views.py
@@ -1,21 +1,10 @@
 from django.contrib.auth import get_user_model
 from rest_framework import viewsets
 from .models import Post
-from . serializers import PostSerializer, UserSerializer
 from .permissions import IsAuthorOrReadOnly
-from django.views.generic import TemplateView
+from . serializers import PostSerializer, UserSerializer
+
 # Create your views here.
-
-
-class HomePageView(TemplateView):
-    template_name = 'home.html'
-
-
-class PostViewSet(viewsets.ModelViewSet):
-    permission_classes = (IsAuthorOrReadOnly,)
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-
 
 class PostViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAuthorOrReadOnly,)
